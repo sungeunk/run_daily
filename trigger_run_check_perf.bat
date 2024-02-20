49e07718a315648bf72520eaf94790c366aade38
Kim, SungEun <sungeun.kim@intel.com>
2024-02-20 22:35:54 +0900

    add trigger_run_check_perf

---
+++ trigger_run_check_perf.bat
@@ -0,0 +1,83 @@
+@echo off && setlocal
+
+:: include user definitions
+call user_definitions_%COMPUTERNAME%.bat
+
+:: set environments
+:: virtualenv
+if not exist %DAILY_ROOT%\venv\Scripts\activate.bat (
+    echo "Please call init_env.bat to set dev environments."
+    exit /b 0
+)
+call %DAILY_ROOT%\venv\Scripts\activate.bat
+
+:: openvino
+if not exist %OV_SETUP_SCRIPT% (
+    echo "could not find %OV_SETUP_SCRIPT%"
+    exit /b 0
+)
+call %OV_SETUP_SCRIPT%
+
+python -c "import importlib;test = importlib.import_module('gpu-tools.run_daily');test.get_now();" > temp.txt
+SET /p DATE= < temp.txt
+del temp.txt
+SET MAIL_FILE=%DW_ROOT%\check_perf.%DATE%.mail
+SET APP=%INTEL_OPENVINO_DIR%\samples\cpp\build\intel64\benchmark_app.exe
+
+:: Begin tag for mail
+echo. ^<pre^> >> %MAIL_FILE%
+
+:: write header for check_perf v1
+(
+echo. ^<pre^>
+echo. ===============
+echo. MODEL V1 result
+echo. ===============
+) >> %MAIL_FILE%
+
+:: run check_perf v1
+python gpu-tools/check_performance.py -d GPU.1 -a %APP% ^
+    -m Z:\models ^
+    -r result.txt --report_load_time load_time.txt ^
+    --cldnn gpu-tools/ref/cldnn.report --ref gpu-tools/ref/onednn.report
+
+:: write results for check_perf v1
+(
+echo.
+type result.txt
+echo.
+type load_time.txt
+) >> %MAIL_FILE%
+
+:: write header for check_perf v2
+(
+echo. ^<pre^>
+echo. ===============
+echo. MODEL V2 result
+echo. ===============
+) >> %MAIL_FILE%
+
+:: run check_perf v2
+python gpu-tools/check_performance.py -d GPU.1 -a %APP% --version 2 --nstreams 4 --use_device_mem ^
+    -m Z:\models ^
+    -r result.txt --report_load_time load_time.txt ^
+    --cldnn gpu-tools/ref/cldnn.v2report --ref gpu-tools/ref/onednn.v2report
+
+:: write results for check_perf v2
+(
+echo.
+type result.txt
+echo.
+type load_time.txt
+) >> %MAIL_FILE%
+
+:: End tag for mail
+echo. ^</pre^> >> %MAIL_FILE%
+
+:: remove temp files.
+del result.txt
+del load_time.txt
+
+:: send mail
+set MAIL_FILE=%MAIL_FILE:\=/% 
+python -c "import importlib;test = importlib.import_module('gpu-tools.run_daily');test.send_mail('%MAIL_FILE%', 'check_perf');"
--- user_definitions_DG2ALDERLAKE.bat
+++ user_definitions_DG2ALDERLAKE.bat
@@ -1,9 +1,9 @@
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: set path (User definitions)
 SET DAILY_ROOT=%~dp0
-SET DW_ROOT="%DAILY_ROOT%\directory_browsing\run_daily"
-SET GPU_TOOLS="%DAILY_ROOT%\py"
-SET MODEL_ROOT="C:\dev\models"
+SET DW_ROOT=%DAILY_ROOT%\directory_browsing\run_daily
+SET GPU_TOOLS=%DAILY_ROOT%\gpu-tools
+SET MODEL_ROOT=C:\dev\models
 SET OCL_ROOT=%DAILY_ROOT%\thirdparty\OpenCL-SDK-v2023.12.14-Win-x64
 SET VC_ENV_FILE="C:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvars64.bat"
 :: End (User definitions)
