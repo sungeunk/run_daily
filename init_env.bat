5ef371c51aed9b088a0cfb2da76274c0daaee516
Kim, SungEun <sungeun.kim@intel.com>
2024-02-21 21:36:21 +0900

    mount

--- init_env.bat
+++ init_env.bat
@@ -12,6 +12,11 @@ cd openvino.genai.qwen
 git submodule update --init --recursive
 cd ..
 
+:: mount model directory
+if not exist Z:\models\resnet_v1.5_50\resnet_v1.5_50_i8.xml (
+    net use Z: \\sshfs.kr\sungeunk@dg2ubuntu.ikor.intel.com\mnt\nvme\shared
+)
+
 :: for python packages
 if not exist %DAILY_ROOT%\venv\Scripts\activate.bat (
     virtualenv venv
