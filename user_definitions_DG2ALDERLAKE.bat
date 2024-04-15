b2c388a7e9660ad733883b3e303bb2b54e74f019
Kim, SungEun <sungeun.kim@intel.com>
2024-04-16 08:46:29 +0900

    update ref for dg2alderlake

--- user_definitions_DG2ALDERLAKE.bat
+++ user_definitions_DG2ALDERLAKE.bat
@@ -7,7 +7,7 @@ SET MODEL_ROOT=C:\dev\models
 SET OCL_ROOT=%DAILY_ROOT%\thirdparty\OpenCL-SDK-v2023.12.14-Win-x64
 SET VC_ENV_FILE="C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 
-SET REF_PICKLE=%DW_ROOT%\none
+SET REF_PICKLE=%DW_ROOT%\20240416_0826.pickle
 
 SET DEVICE=GPU.1
 SET MAIL_TO=nex.nswe.odt.runtime.kor@intel.com
