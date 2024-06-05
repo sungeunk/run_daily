2022d93d676acc681f9d440bff9434966c00bafd
gta <>
2024-06-05 06:10:52 +0000

    add trigger_convert_models.bat

---
+++ trigger_convert_models.bat
@@ -0,0 +1,43 @@
+@echo off && setlocal
+
+:: include user definitions
+if exist user_definitions_%COMPUTERNAME%.bat (
+    call user_definitions_%COMPUTERNAME%.bat
+) else (
+    call user_definitions_default.bat
+)
+
+:: set environments
+if not exist %DAILY_ROOT%\venv\Scripts\activate.bat (
+    echo "Please call init_env.bat to set dev environments."
+    exit /b 0
+)
+call %DAILY_ROOT%\venv\Scripts\activate.bat
+
+
+:: parsing arguments
+goto GETOPTS
+
+:Help
+echo -h this help messages
+echo -ov {ov setup file path}
+exit /b 0
+
+:GETOPTS
+if /I "%1" == "-h" call :Help
+if /I "%1" == "-ov" set OV_SETUP_SCRIPT=%2 & shift
+shift
+if not "%1" == "" goto GETOPTS
+
+if not exist %OV_SETUP_SCRIPT% (
+    echo "could not find %OV_SETUP_SCRIPT%"
+    exit /b 0
+)
+call %OV_SETUP_SCRIPT%
+
+:: convert models
+@echo on
+python %GPU_TOOLS%\run_llm_daily.py ^
+    -m %MODEL_ROOT% ^
+    -d %DEVICE% ^
+    --convert_models
