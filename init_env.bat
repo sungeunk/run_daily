e1763a7f222c124947f4f8d8d1a587086e1e6c30
Kim, SungEun <sungeun.kim@intel.com>
2024-06-19 10:14:37 +0900

    backup report/raw files

--- init_env.bat
+++ init_env.bat
@@ -7,29 +7,6 @@ if exist user_definitions_%COMPUTERNAME%.bat (
     call user_definitions_default.bat
 )
 
-:: update submodules
-cd openvino.genai
-git submodule update --init --recursive
-cd ..
-
-cd openvino.genai.token
-git submodule update --init --recursive
-cd ..
-
-cd openvino.genai.chatglm3
-git submodule update --init --recursive
-cd ..
-
-cd openvino.genai.qwen
-git submodule update --init --recursive
-cd ..
-
-:: mount model directory
-@REM if not exist Z:\models\resnet_v1.5_50\resnet_v1.5_50_i8.xml (
-@REM     net use Z: \\sshfs.kr\sungeunk@dg2ubuntu.ikor.intel.com\mnt\nvme\shared
-@REM )
-
-
 :: for python packages
 if not exist %DAILY_ROOT%\venv\Scripts\activate.bat (
     pip install -q virtualenv
@@ -44,6 +21,5 @@ if exist %DAILY_ROOT%\venv\Scripts\activate.bat (
 
 pip install -r requirements.txt
 pip install -r openvino.genai\llm_bench\python\requirements.txt
-pip uninstall openvino-nightly
 
 :end_script
