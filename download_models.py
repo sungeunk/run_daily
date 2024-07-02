d405430483bff651d8636beb5116407b4facbb27
Kim, SungEun <sungeun.kim@intel.com>
2024-07-02 07:29:43 +0000

    add maximum models: chatglm3/llama2/mistral

--- download_models.py
+++ download_models.py
@@ -24,6 +24,12 @@ DOWNLOAD_URL_LIST = [
     f'http://ov-share-05.sclab.intel.com/cv_bench_cache/{MODEL_DATE}/gemma-7b-it/pytorch/dldt/compressed_weights/OV_FP16-4BIT_DEFAULT/',
 ]
 
+DOWNLOAD_URL_MAXIMUM_LIST = [
+    f'http://ov-share-05.sclab.intel.com/cv_bench_cache/{MODEL_DATE}/llama-2-7b-chat/pytorch/dldt/compressed_weights/OV_FP16-4BIT_MAXIMUM/',
+    f'http://ov-share-05.sclab.intel.com/cv_bench_cache/{MODEL_DATE}/chatglm3-6b/pytorch/dldt/compressed_weights/OV_FP16-4BIT_MAXIMUM/',
+    f'http://ov-share-05.sclab.intel.com/cv_bench_cache/{MODEL_DATE}/mistral-7b-v0.1/pytorch/dldt/compressed_weights/OV_FP16-4BIT_MAXIMUM/',
+]
+
 # c:\dev\models\WW26_llm_2024.3.0-15805-6138d624dc1\stable-diffusion-v1-5\pytorch\dldt\FP16\
 
 def main():
@@ -31,7 +37,7 @@ def main():
     parser.add_argument('-o', '--output', help='output directory', type=Path, default='c:\dev\models' if IS_WINDOWS else '/mnt/shared/models')
     args = parser.parse_args()
 
-    for url in DOWNLOAD_URL_LIST:
+    for url in DOWNLOAD_URL_LIST + DOWNLOAD_URL_MAXIMUM_LIST:
         wget_cmd = f'wget -q --show-progress --no-proxy -c -r --compression=auto --reject=html,tmp -nH --cut-dirs=1 --no-parent -P {args.output} {url}'
         subprocess.call(wget_cmd, shell=True)
 
