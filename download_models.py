909d3583cc715df7c859e52c98509969a0863ca3
Kim, SungEun <sungeun.kim@intel.com>
2024-08-12 17:08:06 +0900

    update models to WW32_optimum & add glm-4-9b

--- download_models.py
+++ download_models.py
@@ -6,43 +6,42 @@ import subprocess
 from pathlib import Path
 
 IS_WINDOWS = platform.system() == 'Windows'
-MODEL_DATE = os.environ["MODEL_DATE"]
-
+SERVER_ROOT = "http://ov-share-05.sclab.intel.com/cv_bench_cache/WW32_llm-optimum_2024.4.0-16283-41691a36b90"
 DOWNLOAD_URL_LIST = [
-    f'http://ov-share-05.sclab.intel.com/cv_bench_cache/{MODEL_DATE}/stable-diffusion-v1-5/pytorch/dldt/FP16/',
-    f'http://ov-share-05.sclab.intel.com/cv_bench_cache/{MODEL_DATE}/stable-diffusion-v1-5/pytorch/dldt/compressed_weights/OV_FP16-INT8_ASYM/',
-    f'http://ov-share-05.sclab.intel.com/cv_bench_cache/{MODEL_DATE}/stable-diffusion-v2-1/pytorch/dldt/FP16/',
-    f'http://ov-share-05.sclab.intel.com/cv_bench_cache/{MODEL_DATE}/stable-diffusion-v2-1/pytorch/dldt/compressed_weights/OV_FP16-INT8_ASYM/',
-    f'http://ov-share-05.sclab.intel.com/cv_bench_cache/{MODEL_DATE}/lcm-dreamshaper-v7/pytorch/dldt/FP16/',
-    f'http://ov-share-05.sclab.intel.com/cv_bench_cache/{MODEL_DATE}/llama-2-7b-chat/pytorch/dldt/compressed_weights/OV_FP16-4BIT_DEFAULT/',
-    f'http://ov-share-05.sclab.intel.com/cv_bench_cache/{MODEL_DATE}/chatglm3-6b/pytorch/dldt/compressed_weights/OV_FP16-4BIT_DEFAULT/',
-    f'http://ov-share-05.sclab.intel.com/cv_bench_cache/{MODEL_DATE}/qwen-7b-chat/pytorch/dldt/compressed_weights/OV_FP16-4BIT_DEFAULT/',
-    f'http://ov-share-05.sclab.intel.com/cv_bench_cache/{MODEL_DATE}/llama-3-8b/pytorch/dldt/compressed_weights/OV_FP16-4BIT_DEFAULT/',
-    f'http://ov-share-05.sclab.intel.com/cv_bench_cache/{MODEL_DATE}/mistral-7b-v0.1/pytorch/dldt/compressed_weights/OV_FP16-4BIT_DEFAULT/',
-    f'http://ov-share-05.sclab.intel.com/cv_bench_cache/{MODEL_DATE}/phi-2/pytorch/dldt/compressed_weights/OV_FP16-4BIT_DEFAULT/',
-    f'http://ov-share-05.sclab.intel.com/cv_bench_cache/{MODEL_DATE}/phi-3-mini-4k-instruct/pytorch/dldt/compressed_weights/OV_FP16-4BIT_DEFAULT/',
-    f'http://ov-share-05.sclab.intel.com/cv_bench_cache/{MODEL_DATE}/gemma-7b-it/pytorch/dldt/compressed_weights/OV_FP16-4BIT_DEFAULT/',
+    f'{SERVER_ROOT}/baichuan2-7b-chat/pytorch/ov/OV_FP16-4BIT_DEFAULT/',
+    f'{SERVER_ROOT}/chatglm3-6b/pytorch/ov/OV_FP16-4BIT_DEFAULT/',
+    f'{SERVER_ROOT}/chatglm3-6b/pytorch/ov/OV_FP16-4BIT_MAXIMUM/',
+    f'{SERVER_ROOT}/gemma-7b-it/pytorch/ov/OV_FP16-4BIT_DEFAULT/',
+    f'{SERVER_ROOT}/gemma-7b-it/pytorch/ov/OV_FP16-4BIT_MAXIMUM/',
+    f'{SERVER_ROOT}/glm-4-9b/pytorch/ov/OV_FP16-4BIT_DEFAULT/',
+    f'{SERVER_ROOT}/llama-2-7b-chat-hf/pytorch/ov/OV_FP16-4BIT_DEFAULT/',
+    f'{SERVER_ROOT}/llama-2-7b-chat-hf/pytorch/ov/OV_FP16-4BIT_MAXIMUM/',
+    f'{SERVER_ROOT}/llama-3-8b/pytorch/ov/OV_FP16-4BIT_DEFAULT/',
+    f'{SERVER_ROOT}/llama-3-8b/pytorch/ov/OV_FP16-4BIT_MAXIMUM/',
+    f'{SERVER_ROOT}/minicpm-1b-sft/pytorch/ov/OV_FP16-4BIT_DEFAULT/',
+    f'{SERVER_ROOT}/mistral-7b-v0.1/pytorch/ov/OV_FP16-4BIT_DEFAULT/',
+    f'{SERVER_ROOT}/mistral-7b-v0.1/pytorch/ov/OV_FP16-4BIT_MAXIMUM/',
+    f'{SERVER_ROOT}/phi-2/pytorch/ov/OV_FP16-4BIT_DEFAULT/',
+    f'{SERVER_ROOT}/phi-2/pytorch/ov/OV_FP16-4BIT_MAXIMUM/',
+    f'{SERVER_ROOT}/phi-3-mini-4k-instruct/pytorch/ov/OV_FP16-4BIT_DEFAULT/',
+    f'{SERVER_ROOT}/phi-3-mini-4k-instruct/pytorch/ov/OV_FP16-4BIT_MAXIMUM/',
+    f'{SERVER_ROOT}/qwen-7b-chat/pytorch/ov/OV_FP16-4BIT_DEFAULT/',
+    f'{SERVER_ROOT}/qwen-7b-chat/pytorch/ov/OV_FP16-4BIT_MAXIMUM/',
+    f'{SERVER_ROOT}/qwen2-7b/pytorch/ov/OV_FP16-4BIT_DEFAULT/',
 ]
 
-DOWNLOAD_URL_MAXIMUM_LIST = [
-    f'http://ov-share-05.sclab.intel.com/cv_bench_cache/{MODEL_DATE}/llama-2-7b-chat/pytorch/dldt/compressed_weights/OV_FP16-4BIT_MAXIMUM/',
-    f'http://ov-share-05.sclab.intel.com/cv_bench_cache/{MODEL_DATE}/chatglm3-6b/pytorch/dldt/compressed_weights/OV_FP16-4BIT_MAXIMUM/',
-    f'http://ov-share-05.sclab.intel.com/cv_bench_cache/{MODEL_DATE}/mistral-7b-v0.1/pytorch/dldt/compressed_weights/OV_FP16-4BIT_MAXIMUM/',
-    f'http://ov-share-05.sclab.intel.com/cv_bench_cache/{MODEL_DATE}/qwen-7b-chat/pytorch/dldt/compressed_weights/OV_FP16-4BIT_MAXIMUM/',
-    f'http://ov-share-05.sclab.intel.com/cv_bench_cache/{MODEL_DATE}/llama-3-8b/pytorch/dldt/compressed_weights/OV_FP16-4BIT_MAXIMUM/',
-    f'http://ov-share-05.sclab.intel.com/cv_bench_cache/{MODEL_DATE}/phi-2/pytorch/dldt/compressed_weights/OV_FP16-4BIT_MAXIMUM/',
-    f'http://ov-share-05.sclab.intel.com/cv_bench_cache/{MODEL_DATE}/phi-3-mini-4k-instruct/pytorch/dldt/compressed_weights/OV_FP16-4BIT_MAXIMUM/',
-    f'http://ov-share-05.sclab.intel.com/cv_bench_cache/{MODEL_DATE}/gemma-7b-it/pytorch/dldt/compressed_weights/OV_FP16-4BIT_MAXIMUM/',
-]
 
-# c:\dev\models\WW26_llm_2024.3.0-15805-6138d624dc1\stable-diffusion-v1-5\pytorch\dldt\FP16\
+SERVER_ROOT = "http://ov-share-05.sclab.intel.com/cv_bench_cache/WW32_llm-optimum_2024.4.0-16283-41691a36b90"
+DOWNLOAD_OTHER_URL_LIST = [
+    f'{SERVER_ROOT}/lcm-dreamshaper-v7/pytorch/ov/OV_FP16-4BIT_DEFAULT/',
+]
 
 def main():
     parser = argparse.ArgumentParser(description="download models" , formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-o', '--output', help='output directory', type=Path, default='c:\dev\models' if IS_WINDOWS else '/mnt/shared/models')
+    parser.add_argument('-o', '--output', help='output directory', type=Path, default='c:\dev\models' if IS_WINDOWS else '/var/www/html/models')
     args = parser.parse_args()
 
-    for url in DOWNLOAD_URL_LIST + DOWNLOAD_URL_MAXIMUM_LIST:
+    for url in DOWNLOAD_URL_LIST + DOWNLOAD_OTHER_URL_LIST:
         wget_cmd = f'wget -q --show-progress --no-proxy -c -r --compression=auto --reject=html,tmp -nH --cut-dirs=1 --no-parent -P {args.output} {url}'
         subprocess.call(wget_cmd, shell=True)
 
