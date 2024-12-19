a725615204db319a352b56e7fedc219253c77d6f
Kim,SungEun <sungeun.kim@intel.com>
2024-12-19 12:23:07 +0000

    remove unused scripts

--- user_definitions_DG2ALDERLAKE.bat
+++
@@ -1,4 +0,0 @@
-call user_definitions_common.bat
-
-SET REF_REPORT=%DW_ROOT%\daily.20240827_1547.2024.3.0-16041-1e3b88e4e3f-releases_2024_3.report
-SET DEVICE=GPU.1
--- user_definitions_DUT4025LNL.bat
+++
@@ -1,4 +0,0 @@
-call user_definitions_common.bat
-
-SET REF_REPORT=%DW_ROOT%\daily.20240610_1306.report
-SET DEVICE=GPU
