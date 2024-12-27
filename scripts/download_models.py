c06ce41a498a4264d2d9dd0c19f32f4a1bc949ec
Kim, SungEun <sungeun.kim@intel.com>
2024-12-27 12:10:07 +0900

    add download_models.py

--- scripts/download_models.py
+++ scripts/download_models.py
@@ -1,49 +1,57 @@
 import argparse
-import os
+import asyncio
 import platform
-import subprocess
 
 from pathlib import Path
+from tqdm.asyncio import tqdm
 
 IS_WINDOWS = platform.system() == 'Windows'
-SERVER_ROOT = "http://ov-share-05.sclab.intel.com/cv_bench_cache/WW32_llm-optimum_2024.4.0-16283-41691a36b90"
-DOWNLOAD_URL_LIST = [
-    f'{SERVER_ROOT}/baichuan2-7b-chat/pytorch/ov/OV_FP16-4BIT_DEFAULT/',
-    f'{SERVER_ROOT}/chatglm3-6b/pytorch/ov/OV_FP16-4BIT_DEFAULT/',
-    f'{SERVER_ROOT}/chatglm3-6b/pytorch/ov/OV_FP16-4BIT_MAXIMUM/',
-    f'{SERVER_ROOT}/gemma-7b-it/pytorch/ov/OV_FP16-4BIT_DEFAULT/',
-    f'{SERVER_ROOT}/gemma-7b-it/pytorch/ov/OV_FP16-4BIT_MAXIMUM/',
-    f'{SERVER_ROOT}/glm-4-9b/pytorch/ov/OV_FP16-4BIT_DEFAULT/',
-    f'{SERVER_ROOT}/llama-2-7b-chat-hf/pytorch/ov/OV_FP16-4BIT_DEFAULT/',
-    f'{SERVER_ROOT}/llama-2-7b-chat-hf/pytorch/ov/OV_FP16-4BIT_MAXIMUM/',
-    f'{SERVER_ROOT}/llama-3-8b/pytorch/ov/OV_FP16-4BIT_DEFAULT/',
-    f'{SERVER_ROOT}/llama-3-8b/pytorch/ov/OV_FP16-4BIT_MAXIMUM/',
-    f'{SERVER_ROOT}/minicpm-1b-sft/pytorch/ov/OV_FP16-4BIT_DEFAULT/',
-    f'{SERVER_ROOT}/mistral-7b-v0.1/pytorch/ov/OV_FP16-4BIT_DEFAULT/',
-    f'{SERVER_ROOT}/mistral-7b-v0.1/pytorch/ov/OV_FP16-4BIT_MAXIMUM/',
-    f'{SERVER_ROOT}/phi-2/pytorch/ov/OV_FP16-4BIT_DEFAULT/',
-    f'{SERVER_ROOT}/phi-2/pytorch/ov/OV_FP16-4BIT_MAXIMUM/',
-    f'{SERVER_ROOT}/phi-3-mini-4k-instruct/pytorch/ov/OV_FP16-4BIT_DEFAULT/',
-    f'{SERVER_ROOT}/phi-3-mini-4k-instruct/pytorch/ov/OV_FP16-4BIT_MAXIMUM/',
-    f'{SERVER_ROOT}/qwen-7b-chat/pytorch/ov/OV_FP16-4BIT_DEFAULT/',
-    f'{SERVER_ROOT}/qwen-7b-chat/pytorch/ov/OV_FP16-4BIT_MAXIMUM/',
-    f'{SERVER_ROOT}/qwen2-7b/pytorch/ov/OV_FP16-4BIT_DEFAULT/',
+SERVER_ROOT = 'http://ov-share-05.sclab.intel.com/cv_bench_cache'
+MODEL_COMMIT = 'WW52_llm-optimum_2025.0.0-17715'
+MODEL_LIST = [
+    'baichuan2-7b-chat',
+    'chatglm3-6b',
+    'gemma-7b-it',
+    'glm-4-9b-chat',
+    'llama-2-7b-chat-hf',
+    'llama-3-8b',
+    'minicpm-1b-sft',
+    'mistral-7b-v0.1',
+    'phi-2',
+    'phi-3-mini-4k-instruct',
+    'qwen-7b-chat',
+    'qwen2-7b',
 ]
-
-
-SERVER_ROOT = "http://ov-share-05.sclab.intel.com/cv_bench_cache/WW32_llm-optimum_2024.4.0-16283-41691a36b90"
-DOWNLOAD_OTHER_URL_LIST = [
-    f'{SERVER_ROOT}/lcm-dreamshaper-v7/pytorch/ov/OV_FP16-4BIT_DEFAULT/',
+MODEL_PRECISION_LIST = [
+    'pytorch/ov/OV_FP16-4BIT_DEFAULT',
 ]
+DOWNLOAD_URL_LIST = [ f'{SERVER_ROOT}/{MODEL_COMMIT}/{model}/{pre}' for model in MODEL_LIST for pre in MODEL_PRECISION_LIST ]
+
+def convert_cmd_for_popen(cmd):
+    return cmd.split() if IS_WINDOWS else cmd
+
+async def async_download_files(url_list, output):
+    semaphore = asyncio.Semaphore(5)
+
+    async def async_download_file(url, output):
+        async with semaphore:
+            wget_cmd = f'wget -q --no-proxy -c -r --compression=auto --reject=html,tmp -nH --cut-dirs=1 --no-parent -P {output} {url}'
+            print(f'call: {wget_cmd}')
+            process = await asyncio.create_subprocess_exec(*convert_cmd_for_popen(wget_cmd), stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+            stdout, stderr = await process.communicate()
+            print(f'done: {wget_cmd}')
+            print(f'stderr: {stderr}')
+
+    await tqdm.gather(
+        *[async_download_file(url, output) for url in url_list]
+    )
 
 def main():
     parser = argparse.ArgumentParser(description="download models" , formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-o', '--output', help='output directory', type=Path, default='c:\dev\models' if IS_WINDOWS else '/var/www/html/models')
     args = parser.parse_args()
 
-    for url in DOWNLOAD_URL_LIST + DOWNLOAD_OTHER_URL_LIST:
-        wget_cmd = f'wget -q --show-progress --no-proxy -c -r --compression=auto --reject=html,tmp -nH --cut-dirs=1 --no-parent -P {args.output} {url}'
-        subprocess.call(wget_cmd, shell=True)
+    asyncio.run(async_download_files(DOWNLOAD_URL_LIST, args.output))
 
 if __name__ == "__main__":
     main()
