b6189659750f363448369c17067c26315edf6308
Kim, SungEun <sungeun.kim@intel.com>
2025-12-02 10:14:52 +0900

    add '--apply_chat_template' for llm models

--- scripts/test_cases/test_benchmark.py
+++ scripts/test_cases/test_benchmark.py
@@ -44,7 +44,7 @@ class TestBenchmark(TestTemplate):
 
             for config in config_list:
                 MODEL_PATH = convert_path(f'{args.model_dir}/{cfg.MODEL_DATE}/{key_tuple[0]}/pytorch/ov/{key_tuple[1]}')
-                cmd = f'python {APP_PATH} -m {MODEL_PATH} -d {args.device} -mc 1 -ic {cfg.out_token_length} -n {cfg.benchmark_iter_num} {"--optimum" if args.optimum else "" }'
+                cmd = f'python {APP_PATH} -m {MODEL_PATH} -d {args.device} -mc 1 --apply_chat_template -ic {cfg.out_token_length} -n {cfg.benchmark_iter_num} {"--optimum" if args.optimum else "" }'
                 if not args.prompt_permutation:
                     cmd += f' --disable_prompt_permutation'
                 if not args.continuous_batch:
